4938d21516e6a4d3de574df490dc592967f24730
Verona0609 <dikaya.nika15@gmail.com>
2025-02-24 18:26:05 +0100

    header adaptive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,39 +6,43 @@ import { Icon } from '../Icon/Icon';
 
 const Header = () => {
   return (
-    <header className={css.box}>
-      <div className={css.logobox}>
-        <Link>
-          <img className={css.logo} src={logo} alt="website logo" />
-        </Link>
-
-        <h2 className={css.title}>chainstaff</h2>
-      </div>
-
-      <Icon className={css.menu} id="icon-Menu" size={28} />
-
-      <ul className={css.navbox}>
-        <li className={css.nav}>
-          <Link to="/">
-            <span>I'm a Company</span>{' '}
+    <div className={css.wrapper}>
+      <header className={css.box}>
+        <div className={css.logobox}>
+          <Link>
+            <img className={css.logo} src={logo} alt="website logo" />
           </Link>
-        </li>
-        <li className={css.nav}>
-          <Link to="/chainstafers">
-            <span> Chainstafer</span>
+
+          <h2 className={css.title}>chainstaff</h2>
+        </div>
+
+        <Icon className={css.menu} id="icon-Menu" size={28} />
+
+        <ul className={css.navbox}>
+          <li className={css.nav}>
+            <Link to="/">
+              <span>I'm a Company</span>{' '}
+            </Link>
+
+            <Icon className={css.angle} id="icon-angle-left" />
+          </li>
+          <li className={css.nav}>
+            <Link to="/chainstafers">
+              <span> Chainstafer</span>
+            </Link>
+          </li>
+          <li className={css.nav}>
+            <Link to="/candidates">Candidate</Link>
+          </li>
+        </ul>
+        <nav className={css.navpage}>
+          <Link to="/aboutus" className={css.nav}>
+            About us
           </Link>
-        </li>
-        <li className={css.nav}>
-          <Link to="/candidates">Candidate</Link>
-        </li>
-      </ul>
-      <nav className={css.navpage}>
-        <Link to="/aboutus" className={css.nav}>
-          About us
-        </Link>
-      </nav>
-      <button className={css.btn}>Get started</button>
-    </header>
+        </nav>
+        <button className={css.btn}>Get started</button>
+      </header>
+    </div>
   );
 };
 
