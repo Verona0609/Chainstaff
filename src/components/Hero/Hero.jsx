b83274a1d5f079081bef9bc36ac6dc2a8d8cda3a
Verona0609 <dikaya.nika15@gmail.com>
2025-02-25 17:21:00 +0100

    hero content

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,13 +3,13 @@ import css from './Hero.module.css';
 const Hero = () => {
   return (
     <div className="container">
-      {/*  <div className={css.content}>
-        <h1 className={css.text}>
+      <div className={css.content}>
+        <h1 className={css.title}>
           Supecharge your <span>hiring strategy</span> through personal networks
         </h1>
+        <p className={css.text}>Streamline your recruitment with chainstaff's referral-based hiring solutions. Discover top talent faster and reduce costs effortlessly.</p>
         <button className={css.btn}>Get started</button>
-      </div> */}
-      {/*  <img src={man} alt="man-hero" className={css.img} /> */}
+      </div>
     </div>
   );
 };
