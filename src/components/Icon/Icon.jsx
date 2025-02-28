1b0f4beb963f2b51a9f8c7aac5be1dbcaa3f8de6
Verona0609 <dikaya.nika15@gmail.com>
2025-02-28 10:23:58 +0100

    steps section

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,9 +1,10 @@
 import Icons from '../../assets/svg/sprite.svg';
+
 console.log(Icons);
 export const Icon = ({ id, className, size }) => {
   return (
     <svg className={className} height={size} width={size}>
-      <use href={Icons + '#' + id}></use>
+      <use href={'/svg/sprite.svg#' + id}></use>
     </svg>
   );
 };
